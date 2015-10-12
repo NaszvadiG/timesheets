8a4b01d5c1a5841a91be8a1ccfb1b2e58f21f9b5
hai <hai@10.0.1.185>
2015-10-12 14:50:29 -0400

    added Handsontable integration

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,107 +2,89 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Welcome to timesheets 1.0</title>
 
-	<style type="text/css">
+        <script src="<?php  echo base_url().'assets/dist/handsontable.full.js' ?>"></script>
+<!--        <script src="<?php //echo base_url().'assets/js/jquery.min.js' ?>"></script>
+-->        <link rel="stylesheet" media="screen"  href="<?php echo base_url().'assets/dist/handsontable.full.css' ?>">
+<!--        <link rel="stylesheet" media="screen" href="http://handsontable.com/dist/handsontable.full.css">-->
+     
 
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-        <script src="http://handsontable.com/dist/handsontable.full.js"></script>
-        <link rel="stylesheet" media="screen" href="http://handsontable.com/dist/handsontable.full.css">
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+    <div id="example"></div>
+    
+    
+      <script type ="text/javascript" >
+            
+function getprojectData() {
+    
+        return [
+  
+  ["TC","10/03/15","PHIL_180","Programming","Programming", 1, 1, 2, 3, 3, 5, 1, 16],
+  ["TC","10/03/15","PHIL_180","Programming","Programming", 2, 1, 1, 1,1, 2, 3, 11],
+  ["TC","10/03/15","PHIL_180","Programming","Programming", 3, 5, 1, 1, 2, 1, 1, 14]
+]
+};
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-        <script type ="text/javascript" >
-        var data = [
-  ["", "Ford", "Volvo", "Toyota", "Honda"],
-  ["2014", 10, 11, 12, 13],
-  ["2015", 20, 11, 14, 13],
-  ["2016", 30, 15, 12, 13]
-];
+function getprojectTasks(projectids) {
+    console.log(projectids);
+   var arr =  [
+       ['EMP_key', 'W.E', 'Project #', 'Task','Task Description','S','M','T','W','T','F','S','Total'],
+            ['save data', 'data erase', 'data dump', 'create links', 'project management', 'data cross', 'call', 'Meetings']];
+        return  arr[projectids];
+  
+            
+  
+  
+               };
 
 var container = document.getElementById('example');
+
 var hot = new Handsontable(container, {
-  data: data,
+  data: getprojectData(),
+  colHeaders: ['EMP_key', 'W.E', 'Project #', 'Task','Task Description','S','M','T','W','T','F','S','Total'],
+  
+    columns: [
+      {type: 'dropdown', source:['TC']},
+      {
+          type: 'date',
+          dateFormat: 'MM/DD/YY',
+          correctFormat: true,
+          defaultDate: '01/01/1900'
+          
+      },
+
+      {
+        type: 'autocomplete',
+        source: ['PHIL_180', 'PHIL_181', 'PHIL_182', 'PHIL_183', 'PHIL_184', 'PHIL_185', 'PHIL_186', 'PHIL_187']
+      },
+      {
+          type: 'autocomplete',
+        source: getprojectTasks(1)
+      },
+      {},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'},
+      {type: 'numeric'}
+    ],
   minSpareRows: 1,
   rowHeaders: true,
-  colHeaders: true,
-  contextMenu: true
+  //colHeaders: false,
+  contextMenu: true,
+  columnSorting: true
 });
+
+
         </script>
-
-        <div id="example"></div>
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</div>
-
 </body>
 </html>
